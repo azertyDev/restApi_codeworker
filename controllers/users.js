eb8e3880b91719192d41e3aea84db19b608891d5
azertyDev <khikmatovumid@mail.ru>
2020-01-13 11:26:32 +0500

    initial commit

--- controllers/users.js
+++ controllers/users.js
@@ -1,24 +1,62 @@
 const User = require('../models/users');
+const Car  = require('../models/car');
 
 module.exports = {
-    index: (req, res, next) => {
-        User.find({})
-        .then(users => {
-            res.status(200).json(users);
-        })
-        .catch(err => {
-            next(err);
-        })
+    index: async (req, res, next) => {
+        const users = await User.find({});
+        res.status(200).json(users);
     },
 
-    newUser: (req, res, next) => {
+    newUser: async (req, res, next) => {
         const newUser = new User(req.body);
-        newUser.save()
-        .then(user => {
-            res.status(200).json(user); 
-        })
-        .catch(err => {
-            next(err);
-        })
+        const user = await newUser.save();
+        res.status(201).json(user);
+    },
+
+    getUser: async (req, res, next) => {
+        const { userId } = req.params;
+        const user = await User.findById( userId );
+        res.status(200).json(user);
+    },
+
+    replaceUser: async (req, res, next) => {
+        const { userId } = req.params;
+        const newUser = req.body;
+        const result = await User.findByIdAndUpdate(userId, newUser);
+        console.log('result: ', result);
+        res.status(200).json({ success: true});
+    },
+
+    updateUser: async (req, res, next) => {
+        const { userId } = req.params;
+        const newUser = req.body;
+        const result = await User.findByIdAndUpdate(userId, newUser);
+        console.log('result: ', result);
+        res.status(200).json({ success: true});
+    },
+
+    getUserCars: async (req, res, next) => {
+        const { userId } = req.params;
+        const user = await User.findById( userId ).populate('cars');
+        console.log('User:', user);
+        res.status(200).json(user.cars);
+    }, 
+    
+    newUserCars: async (req, res, next) => {
+        const { userId } = req.params;
+        // Create a new car
+        const newCar = new Car(req.body);
+        // Get user
+        const user = await User.findById( userId );
+        // Assign user as a car's seller
+        newCar.seller = user;
+        // Save the car
+        await newCar.save();
+        // Add car to the user's selling array 'cars'
+        user.cars.push(newCar);
+        // Save the user
+        await user.save();
+        res.status(201).json(newCar);
+        console.log(newCar);
     }
 };
--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,17 @@ const UsersController = require('../controllers/users');
 
 router.route('/')
     .get(UsersController.index)
-    .post(UsersController.newUser)
+    .post(UsersController.newUser);
  
+// /users/:userId
+router.route('/:userId')
+    .get(UsersController.getUser)
+    .put(UsersController.replaceUser)
+    .patch(UsersController.updateUser);
+
+// /users/:userId/cars
+router.route('/:userId/cars')
+    .get(UsersController.getUserCars)
+    .post(UsersController.newUserCars)
 
 module.exports = router;
